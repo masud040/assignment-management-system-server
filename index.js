929f0840b3a81f3560e40a1c296444a3b62d9126
masudo40 <masudinfo128@gamil.com>
2023-11-05 03:40:45 +0600

    add an api endpoint for get all assignment add level based assignment

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+var jwt = require("jsonwebtoken");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yngtotr.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   serverApi: {
@@ -18,21 +24,48 @@ async function run() {
   try {
     await client.connect();
 
-    const assignmentCollections = client
+    const assignmentCollection = client
       .db("assignmentsDB")
       .collection("assignments");
 
-    //   jwt -> json web token
+    //   jwt -> json web token related
     app.post("/jwt", async (req, res) => {
       const user = req.body;
-      console.log(user);
-      res.send(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1hr",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .send({ success: true });
     });
-    //   create assignment
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      res
+        .clearCookie("token", {
+          maxAge: 0,
+        })
+        .send({ success: true });
+    });
+    // assignment related
+    app.get("/assignments", async (req, res) => {
+      const query = {};
+      const level = req.query.level;
+      if (level) {
+        query.level = level;
+      }
+      console.log(query);
+      const cursor = assignmentCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
     app.post("/create-assignment", async (req, res) => {
-      const assignment = req.body;
-      console.log(assignment);
-      res.send(assignment);
+      const assignment = req.body.assignment;
+      const result = await assignmentCollection.insertOne(assignment);
+      res.send(result);
     });
 
     await client.db("admin").command({ ping: 1 });
